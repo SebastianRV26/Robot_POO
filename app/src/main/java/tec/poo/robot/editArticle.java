37002f3fc1dcdfdac640e01744279e11e0a081d8
Sebastian RV <sebasrv26@gmail.com>
2019-11-01 14:18:28 -0600

    LawyerEdit

---
+++ app/src/main/java/tec/poo/robot/editArticle.java
@@ -0,0 +1,14 @@
+package tec.poo.robot;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class editArticle extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_article);
+    }
+}
